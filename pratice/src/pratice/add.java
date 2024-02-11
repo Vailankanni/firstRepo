4f9322bb65e919aa67199e84c725c228073fbbb7
Vailankanni <freyapereira2227@gmail.com>
2024-02-11 21:06:49 +0530

    first commit

---
+++ pratice/src/pratice/add.java
@@ -0,0 +1,15 @@
+package pratice;
+
+public class add {
+	
+	int no1,no2;
+	
+	public int sum(int no1, int no2) {
+		return no1 + no2;
+	}
+	
+	public int diff (int no1, int no2) {
+		return no1-no2;
+	}
+
+}
