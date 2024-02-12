fd580e3f84e71e4cac3681e9d99b6bc0cb77a111
Vailankanni <freyapereira2227@gmail.com>
2024-02-12 08:48:40 +0530

    second

--- exam/src/exam/news.java
+++ exam/src/exam/news.java
@@ -4,11 +4,24 @@ public class news {
 	
 	float amt,rate;
 	int period;
+	String accno;
 	
 	news(float amt, float rate, int period){
 		this.amt = amt;
 		this.rate = rate;
 		this.period = period;
 	}
-
+	
+	float getamt() {
+		return amt;
+	}
+	
+	float getrate() {
+		return rate;
+	}
+	
+	void display() {
+		float intt = amt * (rate/100)*period;
+		System.out.println("SI: "+intt);
+	}
 }
