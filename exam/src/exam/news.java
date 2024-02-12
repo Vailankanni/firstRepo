705075913d27fc84653debd2e88ac7f5e183f6ba
Vailankanni <freyapereira2227@gmail.com>
2024-02-12 08:41:31 +0530

    first

---
+++ exam/src/exam/news.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class news {
+	
+	float amt,rate;
+	int period;
+	
+	news(float amt, float rate, int period){
+		this.amt = amt;
+		this.rate = rate;
+		this.period = period;
+	}
+
+}
